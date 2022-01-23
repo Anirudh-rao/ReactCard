0098c67bbe267fb459ce6c39b576e6245090abea
Anirudh Rao <65554770+Anirudh-rao@users.noreply.github.com>
2022-01-23 19:39:39 +0530

    Project Done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,9 @@
 import React from "react"
-
+import reactlogo from "../Images/logo512.png";
 export default function Navbar() {
     return (
         <nav>
-            <img src="D:\React\reactcard\src\Images\logo512.png" className="nav--icon" />
+            <img src={reactlogo} className="nav--icon" />
             <h3 className="nav--logo_text">ReactFacts</h3>
             <h4 className="nav--title">React Course - Project 1</h4>
         </nav>
