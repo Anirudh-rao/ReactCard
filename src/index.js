45a1decf387ba46d924d707309d08798cc075687
Anirudh Rao <65554770+Anirudh-rao@users.noreply.github.com>
2022-01-23 19:34:12 +0530

    React app Coustomised

--- src/index.js
+++ src/index.js
@@ -1,25 +1,9 @@
+/* eslint-disable jsx-a11y/alt-text */
 //React imposr
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./App"
 
-
-const page = (
-  <div>
-    <img src="./Images/logo192.png" width="140rem"></img>
-  <h1>Fun Facts About React</h1>
-  <ul>
-  <li>Was originally released in 2013</li>
-  <li>Was originally created by Jordane Walker</li>
-  <li>Has well over 100k stars in Github</li>
-  <li>Is maintained by Facebook</li>
-  <li>Powers thousands of applications , including mobile apps</li>
-  </ul>
-  </div>
-)
-
-
-
-
-ReactDOM.render(page,document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"))
 
 
